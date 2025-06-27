9af7a90d1363de788c90556f81b0ca04cf12b22a
jsundb <johannes.sundback.2002@gmail.com>
2025-06-27 22:25:08 +0300

    Removed avatars from private chat.

--- components/chat-interface.tsx
+++ components/chat-interface.tsx
@@ -195,26 +195,6 @@ export function ChatInterface({
                 key={message.id}
                 className={`flex gap-2 ${message.otherUserName === user.first_name ? "justify-end" : "justify-start"}`}
               >
-                {message.otherUserName !== user.first_name && (
-                  <Avatar className="h-6 w-6">
-                    <AvatarImage 
-                      src={
-                        message.otherUserAvatar
-                          ? message.otherUserAvatar.startsWith("http")
-                            ? message.otherUserAvatar
-                            : `${API_URL}${message.otherUserAvatar}`
-                          : "/placeholder.svg"
-                      } 
-                      alt={message.otherUserName} 
-                    />
-                    <AvatarFallback className="text-xs">
-                      {message.otherUserName
-                        .split(" ")
-                        .map((n) => n[0])
-                        .join("")}
-                    </AvatarFallback>
-                  </Avatar>
-                )}
                 <div
                   className={`max-w-[70%] rounded-lg px-3 py-2 text-sm ${message.otherUserName === user.first_name ? "bg-primary text-primary-foreground" : "bg-muted"
                     }`}
